06d55ddf65188e6b66994dd9314f667130cdb99c
Chad Whitacre <chad@zetaweb.com>
2013-08-08 11:37:05 -0400

    Bump version to 1.0.1

--- postgres.py
+++ postgres.py
@@ -162,7 +162,7 @@ from psycopg2.extras import RealDictCursor
 from psycopg2.pool import ThreadedConnectionPool as ConnectionPool
 
 
-__version__ = '1.0.0-dev'
+__version__ = '1.0.1'
 
 
 # A Helper
