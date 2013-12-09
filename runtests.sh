2093c0a00861ac8b40870a9e18d43bd9cf77f80c
Chad Whitacre <chad@zetaweb.com>
2013-12-09 15:26:45 -0500

    Fix #34
    We now exit 1 iff the pytests fail.

--- runtests.sh
+++ runtests.sh
@@ -1,16 +1,34 @@
 #!/bin/sh
-set -e
-DATABASE_URL=postgres://jrandom@localhost/test py.test tests.py -v
-echo "Starting doctests."
-python postgres/__init__.py
-python postgres/cursors.py
-python postgres/orm.py
-echo ""
-python --version 2>&1 | grep 'Python 3' > /dev/null && \
-    echo "\x1b[31mYou may see errors\x1b[0m due to dict ordering not being stable."
-python --version 2>&1 | grep 'Python 2' > /dev/null && \
-    echo "\x1b[31mYou're using Python 2.\x1b[0m Expect errors comparing '' and u''." && \
-    echo "You may also see errors due to dict ordering not being stable."
-echo "We don't fix these because that would make docs uglier."
-echo ""
-echo "Done with doctests."
+
+# Run both pytests and doctests, but only vary the return code on the result of
+# the pytests. The doctests can fail for spurious reasons, and we chose not to
+# fix them because fixing them would make the docs uglier. As long as the
+# doctests pass for Python 3 then we know the doc examples are good.
+
+function run_pytests() {
+    DATABASE_URL=postgres://jrandom@localhost/test py.test tests.py -v && return 0 || return 1
+}
+
+function run_doctests() {
+    echo "Starting doctests."
+    python postgres/__init__.py
+    python postgres/cursors.py
+    python postgres/orm.py
+    echo ""
+    python --version 2>&1 | grep 'Python 3' > /dev/null && \
+        echo "\x1b[31mYou may see errors\x1b[0m due to dict ordering not being stable."
+    python --version 2>&1 | grep 'Python 2' > /dev/null && \
+        echo "\x1b[31mYou're using Python 2.\x1b[0m Expect errors comparing '' and u''." && \
+        echo "You may also see errors due to dict ordering not being stable."
+    echo "We don't fix these because that would make docs uglier."
+    echo ""
+    echo "Done with doctests."
+
+    return 0  # Always report success, because we don't want Travis to choke on u''.
+}
+
+run_pytests
+SUCCESS=$?
+
+run_doctests
+exit $SUCCESS
