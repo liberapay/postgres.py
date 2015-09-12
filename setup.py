a890ad4685bcef1e9de71f94a8fa1ba03fee114b
Chad Whitacre <chad@zetaweb.com>
2015-09-12 09:58:07 -0400

    Bump version to 2.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup( name='postgres'
      , author='Gittip, LLC'
      , description="postgres is a high-value abstraction over psycopg2."
      , url='https://postgres-py.readthedocs.org'
-     , version='2.1.2-dev'
+     , version='2.2.0'
      , packages=find_packages()
      , install_requires=['psycopg2 >= 2.5.0']
      , classifiers=[
