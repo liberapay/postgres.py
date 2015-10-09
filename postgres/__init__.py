0c72d6e8d74f2095f40c87e40e88ae2c69bde505
Chad Whitacre <chad@zetaweb.com>
2015-10-09 19:51:39 -0400

    Bump version to 2.2.1

--- postgres/__init__.py
+++ postgres/__init__.py
@@ -185,7 +185,7 @@ from psycopg2.extras import register_composite, CompositeCaster
 from psycopg2.pool import ThreadedConnectionPool as ConnectionPool
 
 
-__version__ = '2.2.0-dev'
+__version__ = '2.2.1'
 
 
 # A Helper
