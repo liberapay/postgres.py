25108ae3ade7653efa0339a85cf0ccce1d6d74df
Changaco <changaco@changaco.oy.lc>
2019-10-08 13:29:01 +0200

    fix the `Connection.get_cursor` method

--- postgres/context_managers.py
+++ postgres/context_managers.py
@@ -49,6 +49,48 @@ class CursorContextManager(object):
         self.pool.putconn(self.conn)
 
 
+class ConnectionCursorContextManager(object):
+    """Creates a cursor from the given connection, then wraps it in a context
+    manager that automatically commits or rolls back the changes on exit.
+
+    :param conn: a :class:`psycopg2:connection`
+    :param bool autocommit: see :attr:`psycopg2:connection.autocommit`
+    :param bool readonly: see :attr:`psycopg2:connection.readonly`
+    :param \**cursor_kwargs: passed to :meth:`psycopg2:connection.cursor`
+
+    During construction, the connection's :attr:`autocommit` and :attr:`readonly`
+    attributes are set, then :meth:`psycopg2:connection.cursor` is called with
+    `cursor_kwargs`.
+
+    Upon exit of the ``with`` block, the connection is rolled back if an
+    exception was raised, or committed otherwise. There are two exceptions to
+    this:
+
+    1. if :attr:`autocommit` is :obj:`True`, then the connection is neither
+       rolled back nor committed;
+    2. if :attr:`readonly` is :obj:`True`, then the connection is always rolled
+       back, never committed.
+
+    In all cases the cursor is closed.
+
+    """
+
+    __slots__ = ('conn', 'cursor')
+
+    def __init__(self, conn, autocommit=False, readonly=False, **cursor_kwargs):
+        conn.autocommit = autocommit
+        conn.readonly = readonly
+        self.conn = conn
+        self.cursor = conn.cursor(**cursor_kwargs)
+
+    def __enter__(self):
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.cursor.close()
+        self.conn.__exit__(exc_type, exc_val, exc_tb)
+
+
 class CursorSubcontextManager(object):
     """Wraps a cursor so that it can be used for a subtransaction.
 
